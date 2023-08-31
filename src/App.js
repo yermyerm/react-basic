0cb6684121f089218de0b76fce2d4ffe73677547
yermyerm <songkyuu@gmail.com>
2023-08-31 22:36:14 +0900

    6.1 수강중

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((current) => current + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text="yayyy" />
+      <h1>{counter}</h1>
+      <Button text="Click" onClick={onClick} />
     </div>
   );
 }
